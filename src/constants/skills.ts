71b109729ddda566458010f5f590fbe2fedaeb9d
ArielSurco <arielsurcoa@gmail.com>
2025-01-22 00:10:01 -0300

    feat: finish skills section

---
+++ src/constants/skills.ts
@@ -0,0 +1,63 @@
+export interface Skill {
+  src: string
+  title: string
+}
+
+export const skills: Skill[] = [
+  {
+    src: '/assets/icons/nextjs.svg',
+    title: 'NextJS'
+  },
+  {
+    src: '/assets/icons/reactjs.svg',
+    title: 'React'
+  },
+  {
+    src: '/assets/icons/astro.svg',
+    title: 'Astro'
+  },
+  {
+    src: '/assets/icons/express.svg',
+    title: 'Express'
+  },
+  {
+    src: '/assets/icons/nodejs.svg',
+    title: 'NodeJS'
+  },
+  {
+    src: '/assets/icons/mongo.svg',
+    title: 'MongoDB'
+  },
+  {
+    src: '/assets/icons/typescript.svg',
+    title: 'TypeScript'
+  },
+  {
+    src: '/assets/icons/javascript.svg',
+    title: 'JavaScript'
+  },
+  {
+    src: '/assets/icons/tailwind.svg',
+    title: 'Tailwind'
+  },
+  {
+    src: '/assets/icons/sass.svg',
+    title: 'Sass'
+  },
+  {
+    src: '/assets/icons/bootstrap.png',
+    title: 'Bootstrap'
+  },
+  {
+    src: '/assets/icons/git.svg',
+    title: 'Git'
+  },
+  {
+    src: '/assets/icons/html.svg',
+    title: 'HTML'
+  },
+  {
+    src: '/assets/icons/css.svg',
+    title: 'CSS'
+  },
+]
