24396a9530b490559bb0543ea7031dadc528ca21
ArielSurco <arielsurcoa@gmail.com>
2025-07-19 13:06:27 -0300

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: false
     },
   },
+  adapter: vercel(),
 });
