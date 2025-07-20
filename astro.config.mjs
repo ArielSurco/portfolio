051f6765858289d5c12f654ee312bd84254af309
ArielSurco <arielsurcoa@gmail.com>
2025-07-19 22:22:27 -0300

    fix: try to avoid blank page on locale redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,14 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: true,
+      redirectToDefaultLocale: false
     },
   },
   adapter: vercel(),
+  output: 'server'
 });
