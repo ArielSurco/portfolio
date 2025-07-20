f1c09e563d8abeae40a7a5c825727f3363e043d3
ArielSurco <arielsurcoa@gmail.com>
2025-07-19 23:04:40 -0300

    feat: try again to use vercel adapater

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,6 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
+  output: 'server',
+  adapter: vercel()
 });
